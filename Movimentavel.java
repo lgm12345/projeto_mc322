0774eeb4e84ac23c8806640fc80117ad3729c3f8
lgm12345 <lucasgugelmaciel@gmail.com>
2024-05-19 20:31:08 -0300

    pequenas alteracoes

--- Movimentavel.java
+++ Movimentavel.java
@@ -2,4 +2,5 @@ public interface Movimentavel {
     boolean checknsave(Tabuleiro tabuleiro,int X,int Y) ;
     void listfreepositions(Tabuleiro tabuleiro, int X, int Y) ;
     void resetpositions(Tabuleiro tabuleiro, int X, int Y) ;
+    int[][] movmap() ;
 }
